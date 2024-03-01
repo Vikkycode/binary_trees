92bcd46842b9bc02940066d330e51719165bc174
Victor <vicky.robilson5528@gmail.com>
2024-02-29 20:37:46 -0800

    131-heap_insert

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -9,10 +9,9 @@
  *         NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
-
 {
 	heap_t *tree, *new, *flip;
-	int size, sub, bit, level, tmp_t, leaves;
+	int size, leaves, sub, bit, level, tmp;
 
 	if (!root)
 		return (NULL);
@@ -23,18 +22,19 @@ heap_t *heap_insert(heap_t **root, int value)
 	leaves = size;
 	for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
 		leaves -= sub;
+	/* subtract all nodes except for bottom-most level */
 
 	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
 		tree = leaves & bit ? tree->right : tree->left;
-/*
- * Traverse tree to first empty slot based on the binary
- * representation of the number of leaves.
- * Example -
- * If there are 12 nodes in a complete tree, there are 5 leaves on
- * the 4th tier of the tree. 5 is 101 in binary. 1 corresponds to
- * right, 0 to left.
- * The first empty node is 101 == RLR, *root->right->left->right
-*/
+	/*
+	 * Traverse tree to first empty slot based on the binary
+	 * representation of the number of leaves.
+	 * Example -
+	 * If there are 12 nodes in a complete tree, there are 5 leaves on
+	 * the 4th tier of the tree. 5 is 101 in binary. 1 corresponds to
+	 * right, 0 to left.
+	 * The first empty node is 101 == RLR, *root->right->left->right
+	 */
 
 	new = binary_tree_node(tree, value);
 	leaves & 1 ? (tree->right = new) : (tree->left = new);
@@ -42,23 +42,23 @@ heap_t *heap_insert(heap_t **root, int value)
 	flip = new;
 	for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
 	{
-		tmp_t = flip->n;
+		tmp = flip->n;
 		flip->n = flip->parent->n;
-		flip->parent->n = tmp_t;
+		flip->parent->n = tmp;
 		new = new->parent;
 	}
+	/* Flip values with parent until parent value exceeds new value */
 
 	return (new);
 }
 
 /**
- * binary_tree_size function measures the size of a binary tree
- * @tree: tree to measure
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree to measure the size of
  *
  * Return: size of the tree
- * 0 if tree is NULL
+ *         0 if tree is NULL
  */
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
