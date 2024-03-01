b95c0263ab141996d7016378662acdc6b377da7c
Victor <vicky.robilson5528@gmail.com>
2024-02-29 20:35:29 -0800

    130-binary_tree_is_heap

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,15 +1,13 @@
 #include "binary_trees.h"
 #include "102-binary_tree_is_complete.c"
-
 /**
- * check_max -goes through a binary tree to check ropt as max value
- * @tree: pointer to the root_node
- * Return: 1 if all nodes are max, otherwise 0.
+ * check_max - goes through a binary tree cheking ropt as max value
+ * @tree: pointer to the root
+ * Return: 1 if all nodes are max, 0 in other cases
  **/
 int check_max(const binary_tree_t *tree)
 {
-	int tmp = 1;
-	int tmp_t = 1;
+	int tmp1 = 1, tmp2 = 1;
 
 	if (!tree)
 		return (0);
@@ -19,30 +17,30 @@ int check_max(const binary_tree_t *tree)
 	{
 		if (tree->n <= tree->left->n)
 			return (0);
-		tmp = check_max(tree->left);
+		tmp1 = check_max(tree->left);
 	}
 	if (tree->right)
 	{
 		if (tree->n <= tree->right->n)
 			return (0);
-		tmp_t = check_max(tree->right);
+		tmp2 = check_max(tree->right);
 	}
-	return (tmp && tmp_t);
+	return (tmp1 && tmp2);
 }
 /**
- * binary_tree_is_heap -checks if a binary tree is heap
- * @tree: pointer to node_node
- * Return: 1 in case BTS, otherwise 0
+ * binary_tree_is_heap - checks if a binary tree is heap
+ * @tree: pointer to the node
+ * Return: 1 in case BTS /  0 otherwise
  **/
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-	int tmp_r;
+	int tmp;
 
 	if (!tree)
 		return (0);
 
-	tmp_r = binary_tree_is_complete(tree);
-	if (!tmp_r)
+	tmp = binary_tree_is_complete(tree);
+	if (!tmp)
 		return (0);
 	return (check_max(tree));
 }
